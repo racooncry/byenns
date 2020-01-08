42408dc4b218dbf5413505bfa134c39e2b5d21fe
yangxw123 <yangxw@ikangtai.com>
2020-01-08 09:46:57 +0800

    1、增加线程池

--- src/main/java/com/racoon/yxw/bye996/Bye996Application.java
+++ src/main/java/com/racoon/yxw/bye996/Bye996Application.java
@@ -2,12 +2,27 @@ package com.racoon.yxw.bye996;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Bye996Application {
+@RestController
+public class Bye996Application extends SpringBootServletInitializer {
+    // extends SpringBootServletInitializer
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Bye996Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Bye996Application.class, args);
     }
 
+    @GetMapping("/test")
+    public String test(){
+        return "123";
+    }
 }
